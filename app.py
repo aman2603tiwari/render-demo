54c231cb01b1a2eb7377c482f35dadddc5c21d17
aman2603tiwari <aman_t@ece.iitr.ac.in>
2024-07-16 19:44:01 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+# Load the model and CountVectorizer
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+with open('cv.pkl', 'rb') as cv_file:
+    cv = pickle.load(cv_file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form['text']
+    transformed_text = cv.transform([text])
+    prediction = model.predict(transformed_text)
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
